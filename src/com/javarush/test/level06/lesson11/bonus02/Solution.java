a65af6c5e146927bf53b8e99acdac3acf65590b3
Tsova Alexsander <Tsova@bk.ru>
2016-07-31 21:59:40 +0300

    homework

--- src/com/javarush/test/level06/lesson11/bonus02/Solution.java
+++ src/com/javarush/test/level06/lesson11/bonus02/Solution.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 
 /* Нужно добавить в программу новую функциональность
 Задача: У каждой кошки есть имя и кошка-мама. Создать класс, который бы описывал данную ситуацию. Создать два объекта:
- кошку-дочь и кошку-маму. Вывести их на экран.
+кошку-дочь и кошку-маму. Вывести их на экран.
 Новая задача: У каждой кошки есть имя, кошка-папа и кошка-мама. Изменить класс Cat так, чтобы он мог описать данную ситуацию.
 Создать 6 объектов: дедушку(папин папа), бабушку(мамина мама), папу, маму, сына, дочь.
 Вывести их всех на экран в порядке: дедушка, бабушка, папа, мама, сын, дочь.
@@ -30,43 +30,65 @@ Cat name is дочь Пушинка, mother is мама Василиса, father
 
 public class Solution
 {
-    public static void main(String[] args) throws IOException
+   public static void main(String[] args) throws IOException
     {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        String grFatherName = reader.readLine();
+        Cat catGrFather = new Cat(grFatherName);
+
+        String grMotherName = reader.readLine();
+        Cat catGrMother = new Cat(grMotherName);
+
+        String FatherName = reader.readLine();
+        Cat catFather = new Cat(FatherName, catGrFather, null);
+
         String motherName = reader.readLine();
-        Cat catMother = new Cat(motherName);
+        Cat catMother = new Cat(motherName, null, catGrMother);
+
+        String sonName = reader.readLine();
+        Cat catSon = new Cat(sonName, catFather, catMother);
 
         String daughterName = reader.readLine();
-        Cat catDaughter = new Cat(daughterName, catMother);
+        Cat catDaughter = new Cat(daughterName, catFather, catMother);
 
+        System.out.println(catGrFather);
+        System.out.println(catGrMother);
+        System.out.println(catFather);
         System.out.println(catMother);
+        System.out.println(catSon);
         System.out.println(catDaughter);
     }
 
     public static class Cat
     {
         private String name;
-        private Cat parent;
+        private Cat Father;
+        private Cat Mother;
 
         Cat(String name)
         {
             this.name = name;
         }
 
-        Cat(String name, Cat parent)
-        {
-            this.name = name;
-            this.parent = parent;
+        Cat(String name, Cat Father, Cat Mother){
+            this.name=name;
+            this.Father=Father;
+            this.Mother=Mother;
         }
 
         @Override
-        public String toString()
-        {
-            if (parent == null)
-                return "Cat name is " + name + ", no mother ";
+        public String toString() {
+            if (Mother == null && Father == null)
+                return "Cat name is " + name + ", no mother, no father";
+            else if (Mother == null)
+                return "Cat name is " + name + ", no mother, father is " + Father.name;
+            else if (Father == null)
+                return "Cat name is " + name + ", mother is " + Mother.name + ", no father";
             else
-                return "Cat name is " + name + ", mother is " + parent.name;
+                return "Cat name is " + name + ", mother is " + Mother.name + ", father is " + Father.name;
+
+
         }
     }
 
--- src/com/javarush/test/level06/lesson11/home05/Solution.java
+++ src/com/javarush/test/level06/lesson11/home05/Solution.java
@@ -11,8 +11,20 @@ public class Solution
 {
     public static void main(String[] args)
     {
-       // printIdea(new Idea());
+       printIdea(new Idea());
+
+    }
+
+    public static void printIdea(Idea idea){
+        System.out.println(idea.getDescription());
     }
 
     //Добавьте тут класс Idea
+    public static class Idea{
+
+        public String getDescription(){
+            return  "test idea";
+        }
+
+    }
 }
