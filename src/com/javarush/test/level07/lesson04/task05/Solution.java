04f36e4e68b8b75221a2eb8b2d54ed9c083794aa
Tsova Alexsander <Tsova@bk.ru>
2016-08-07 00:27:50 +0300

    home do

--- src/com/javarush/test/level07/lesson04/task05/Solution.java
+++ src/com/javarush/test/level07/lesson04/task05/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.test.level07.lesson04.task05;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /* Один большой массив и два маленьких
 1. Создать массив на 20 чисел.
@@ -16,6 +17,24 @@ public class Solution
     public static void main(String[] args) throws Exception
     {
         //напишите тут ваш код
+        Scanner cr = new Scanner(System.in);
+        int[] mas1 = new int[20];
+
+        for (int i =0; i<mas1.length; i++){
+            mas1[i]=cr.nextInt();
+        }
+
+        int[] mas2 = new int[10];
+        int[] mas3 = new int[10];
+
+        for (int i = 0; i<mas2.length; i++){
+            mas2[i] = mas1[i];
+        }
+        for (int j =0, i=mas3.length; i<mas1.length; i++){
+            mas3[j]=mas1[i];
+            //j+=1;
+            System.out.println(mas3[j]);
+        }
 
     }
 }
