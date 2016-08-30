f8a01659e8d56c1874fe52116e2ec5c283a1c485
Tsova Alex <a.tsova@loopme.com>
2016-08-30 09:44:53 +0300

    do before night

--- src/com/javarush/test/level08/lesson08/task05/Solution.java
+++ src/com/javarush/test/level08/lesson08/task05/Solution.java
@@ -28,31 +28,52 @@ public class Solution
         list.put("Ivanov7", "Ivan7");
         list.put("Ivanov8", "Ivan8");
         list.put("Ivanov9", "Ivan9");
-        list.put("Ivanov10", "Ivan10");
+        list.put("Ivanov10", "Ivan3");
         return list;
     }
 
     public static void removeTheFirstNameDuplicates(HashMap<String, String> map)
     {
         //напишите тут ваш код
-        Iterator<Map.Entry<String, String>> iterator = map.entrySet().iterator();
-        ArrayList<String> list = new ArrayList<String>();
-        while (iterator.hasNext()){
-            Map.Entry<String, String> pair = iterator.next();
-            list.add(pair.getValue());
-        }
-        System.out.println(list);
-        for (int i = 0; i <list.size(); i++){
-            Iterator<Map.Entry<String, String>> iterator2 = map.entrySet().iterator();
-            String name = list.get(i);
-            while (iterator2.hasNext()){
-                Map.Entry<String, String> pair = iterator2.next();
-                String fName = pair.getValue();
-                if (fName.equals(name)){
-                    removeItemFromMapByValue(map, fName);
-                }
+
+        HashMap<String, String> map2 = new HashMap<String, String>(map);
+        HashMap<String, String> map3 = new HashMap<String, String>(map);
+
+        //цикл по map2
+        for (Map.Entry<String, String> pair : map2.entrySet())
+        {
+            //удаляем из map3 пару, которая соответствует pair
+            map3.remove(pair.getKey());
+            //проверяем на наличе  в map3 значений соответсвующего значению из pair
+            if (map3.containsValue(pair.getValue()))
+            {   //Если есть такие же значения - вызываем удаления текующего значения pair из переданной map
+                removeItemFromMapByValue(map, pair.getValue());
             }
         }
+
+//        HashMap<String, String> list2 = new HashMap<String, String>(map);
+//        Iterator<Map.Entry<String, String>> iterator = map.entrySet().iterator();
+//        ArrayList<String> list = new ArrayList<String>();
+//        while (iterator.hasNext()){
+//            Map.Entry<String, String> pair = iterator.next();
+//            list.add(pair.getValue());
+//        }
+//        System.out.println(map);
+//        for (int i = 0; i <list.size(); i++){
+//            Iterator<Map.Entry<String, String>> iterator2 = list2.entrySet().iterator();
+//            String name = list.get(i);
+//            int count = 0;
+//            while (iterator2.hasNext()){
+//                Map.Entry<String, String> pair = iterator2.next();
+//                String fName = pair.getValue();
+//                if (fName.equals(name)){
+//                    count++;
+//                }
+//                if (count > 1){
+//                    removeItemFromMapByValue(map, fName);
+//                }
+//            }
+//        }
         System.out.println(map);
 
     }
