a8876c0f0bb55954e1ca1e8fcb14f6aa68627b7a
Tsova Alex <a.tsova@loopme.com>
2016-09-01 15:13:23 +0300

    do on work

--- src/com/javarush/test/level09/lesson08/task03/Solution.java
+++ src/com/javarush/test/level09/lesson08/task03/Solution.java
@@ -8,7 +8,7 @@ import java.nio.file.FileSystemException;
 1. Разберись, какие исключения бросает метод BEAN.methodThrowExceptions.
 2. Метод processExceptions должен вызывать метод BEAN.methodThrowExceptions и обрабатывать исключения:
 2.1. если возникло исключение FileSystemException, то логировать его (вызвать метод BEAN.log) и пробросить дальше
-2.2. если возникло исключение CharConversionException или любое другое IOException, то только логировать его - вызвать метод BEAN.log
+2.2. если возникло исключение CharConversionExceptio или любое другое IOException, то только логировать его - вызвать метод BEAN.log
 3. Добавь в сигнатуру метода processExceptions класс исключения, которое ты пробрасываешь в п.2.1.
 4. В методе main обработай оставшееся исключение - логируй его. Используй try..catch
 
@@ -23,11 +23,29 @@ public class Solution {
     public static StatelessBean BEAN = new StatelessBean();
 
     public static void main(String[] args) {
-        processExceptions();
+        try {
+            processExceptions();
+        }
+        catch (FileSystemException e){
+                BEAN.log(e);
+        }
     }
 
-    public static void processExceptions() {
-//        BEAN.methodThrowExceptions();
+    public static void processExceptions() throws FileSystemException {
+        try {
+            BEAN.methodThrowExceptions();
+        }
+        catch (FileSystemException e){
+            BEAN.log(e);
+            throw e;
+        }
+        catch (CharConversionException e){
+             BEAN.log(e);
+        }
+        catch (IOException e){
+            BEAN.log(e);
+        }
+
     }
 
     public static class StatelessBean {
